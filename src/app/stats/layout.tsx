f14541844eb0c6462c7191887d52db2edf7b1b8a
ley0x <28061618+ley0x@users.noreply.github.com>
2025-03-20 21:14:31 +0100

    feat : breadcrumb navbar

--- src/app/stats/layout.tsx
+++ src/app/stats/layout.tsx
@@ -8,7 +8,7 @@ export const metadata: Metadata = {
   description: "The Rhythm of Your Life, Quantified",
 };
 
-export default function RootLayout({
+export default async function RootLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
--- src/components/layout/navbar.tsx
+++ src/components/layout/navbar.tsx
@@ -9,24 +9,81 @@ import {
   BreadcrumbSeparator,
 } from "@/components/ui/breadcrumb"
 
-import { SidebarTrigger } from "../ui/sidebar";
+import { SidebarTrigger } from "@/components/ui/sidebar";
+import { usePathname } from "next/navigation";
+
+type BreadcrumbProps = {
+  page: string;
+  username: string | null;
+  subpage: string | null;
+}
+
+const BreadcrumbNav = ({ page, username, subpage }: BreadcrumbProps) => {
+  if (!!subpage) return (
+    <>
+      <BreadcrumbItem>
+        <BreadcrumbLink href={`/`} className="capitalize">Last Beat</BreadcrumbLink>
+      </BreadcrumbItem>
+      <BreadcrumbSeparator />
+      <BreadcrumbItem>
+        <BreadcrumbLink href={`/${page}`} className="capitalize">{page}</BreadcrumbLink>
+      </BreadcrumbItem>
+      <BreadcrumbSeparator />
+      <BreadcrumbItem>
+        <BreadcrumbLink href={`/${page}/${username}`}>{username}</BreadcrumbLink>
+      </BreadcrumbItem>
+      <BreadcrumbSeparator />
+      <BreadcrumbItem>
+        <BreadcrumbPage className="capitalize">{subpage}</BreadcrumbPage>
+      </BreadcrumbItem>
+    </>)
+
+  if (!!username) return (
+    <>
+      <BreadcrumbItem>
+        <BreadcrumbLink href={`/`} className="capitalize">Last Beat</BreadcrumbLink>
+      </BreadcrumbItem>
+      <BreadcrumbSeparator />
+      <BreadcrumbItem>
+        <BreadcrumbLink href={`/${page}`} className="capitalize">{page}</BreadcrumbLink>
+      </BreadcrumbItem>
+      <BreadcrumbSeparator />
+      <BreadcrumbItem>
+        <BreadcrumbPage>{username}</BreadcrumbPage>
+      </BreadcrumbItem>
+    </>
+  )
+  if (!!page) return (
+    <>
+      <BreadcrumbItem>
+        <BreadcrumbLink href={`/`} className="capitalize">Last Beat</BreadcrumbLink>
+      </BreadcrumbItem>
+      <BreadcrumbSeparator />
+      <BreadcrumbItem>
+        <BreadcrumbPage className="capitalize">{page}</BreadcrumbPage>
+      </BreadcrumbItem>
+    </>
+  )
+  return (
+    <BreadcrumbItem>
+      <BreadcrumbPage className="capitalize">Last Beat</BreadcrumbPage>
+    </BreadcrumbItem>
+  )
+}
 export const Navbar = () => {
+  const pathname = usePathname();
+
+  const parsedPathname = pathname.split("/").filter(elt => elt.length > 0)
+  const page = parsedPathname[0];
+  const username = parsedPathname[1] || null;
+  const subpage = parsedPathname[2] || null;
+
   return (
     <nav className="flex gap-5 items-center bg-primary-foreground px-5 py-2 w-full border-b border-sidebar-border">
       <SidebarTrigger />
       <Breadcrumb>
         <BreadcrumbList>
-          <BreadcrumbItem>
-            <BreadcrumbLink href="/stats">Stats</BreadcrumbLink>
-          </BreadcrumbItem>
-          <BreadcrumbSeparator />
-          <BreadcrumbItem>
-            <BreadcrumbLink href="/stats/ley0x">ley0x</BreadcrumbLink>
-          </BreadcrumbItem>
-          <BreadcrumbSeparator />
-          <BreadcrumbItem>
-            <BreadcrumbPage>Albums</BreadcrumbPage>
-          </BreadcrumbItem>
+          <BreadcrumbNav page={page} username={username} subpage={subpage} />
         </BreadcrumbList>
       </Breadcrumb>
     </nav>
