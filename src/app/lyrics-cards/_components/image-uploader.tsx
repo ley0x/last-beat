27fcc3acd0736ac4b1e122a95625959e5383ec76
ley0x <28061618+ley0x@users.noreply.github.com>
2025-04-22 23:35:38 +0200

    feat: implement image uploader component with validation and error handling for image uploads in lyrics cards

--- src/app/lyrics-cards/_components/image-uploader.tsx
+++ src/app/lyrics-cards/_components/image-uploader.tsx
@@ -1,5 +1,74 @@
+'use client';
+
+import React, { useRef, useState, ChangeEventHandler } from 'react';
+import { arrayBufferToString } from '@/lib/utils';
+import { useAtom } from 'jotai';
+import { z } from 'zod';
+import { lcLyricsBackground } from '@/lib/store';
+
+import { File } from 'lucide-react';
+import { Input } from '@/components/ui/input';
+import { Label } from '@/components/ui/label';
+
+const MAX_SIZE = 50 * 1024 * 1024; // 50 MB
+
 export const ImageUploader = () => {
+  const inputRef = useRef<HTMLInputElement>(null);
+  const [, setSelectedImage] = useAtom(lcLyricsBackground);
+  const [invalid, setInvalid] = useState(false);
+  const [error, setError] = useState('');
+
+  const handleImageChange: ChangeEventHandler<HTMLInputElement> = (e) => {
+    if (!e.target.files) return;
+    const file = e.target.files[0];
+
+    if (file.size > MAX_SIZE) {
+      setInvalid(true);
+      setError('Image trop lourde');
+      return;
+    }
+    if (!file.type.startsWith('image/')) {
+      setInvalid(true);
+      setError("Ce n'est pas une image.");
+      return;
+    }
+
+    const reader = new FileReader();
+    reader.onload = () => {
+      if (!reader.result) {
+        setInvalid(true);
+        setError('Impossible de lire le fichier.');
+        return;
+      }
+      const result = z.string().safeParse(reader.result);
+      if (result.success) {
+        setSelectedImage(result.data);
+      } else {
+        const arrayBuffer = reader.result as ArrayBuffer;
+        const string = arrayBufferToString(arrayBuffer);
+        setSelectedImage(string);
+      }
+    };
+    reader.readAsDataURL(file);
+  };
+
   return (
-    <div></div>
-  )
-}
+    <div className="w-full max-w-xs">
+      <div className="space-y-2 w-full">
+        <div className="grid w-full max-w-sm items-center gap-1.5">
+          <Label htmlFor="image-upload"><File className="text-xl" /> Select your background image</Label>
+          <div className="relative">
+            <Input
+              ref={inputRef}
+              id="image-upload"
+              type="file"
+              accept="image/*"
+              onChange={handleImageChange}
+            />
+          </div>
+          {invalid && <p className="text-sm text-red-500">{error}</p>}
+        </div>
+      </div>
+    </div>
+  );
+};
--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -23,7 +23,7 @@ export const fetchUserTopTracks = async (username: string, timeframe: Timeframe,
   const data = LastFmTopTracks.array().parse(json.data);
   const respLimit = z.number().parse(json.limit)
   const respPage = z.number().parse(json.page)
-  return {data, limit: respLimit, page: respPage};
+  return { data, limit: respLimit, page: respPage };
 }
 
 
@@ -53,3 +53,51 @@ export const findLargestImage = (images: z.infer<typeof LastFmImage>[]) => {
   if (!largestImage) return "#";
   return largestImage;
 }
+
+
+
+export const arrayBufferToString = (buffer: ArrayBuffer) => {
+  const decoder = new TextDecoder('utf-8');
+  const decodedString = decoder.decode(buffer);
+  return decodedString;
+};
+
+
+export const copyImageToClipboard = async (link: string) => {
+  try {
+    // Create an image element
+    const img = new Image();
+    img.crossOrigin = "Anonymous"; // Set CORS to anonymous to handle cross-origin images
+
+    // Wait for the image to load
+    await new Promise<HTMLImageElement>((resolve, reject) => {
+      img.onload = () => resolve(img);
+      img.onerror = () => reject(new Error('Failed to load image'));
+      img.src = link;
+    });
+
+    // Create a canvas and draw the image
+    const canvas = document.createElement('canvas');
+    canvas.width = img.width;
+    canvas.height = img.height;
+    const context = canvas.getContext('2d');
+    if (!context) {
+      throw new Error('Failed to get canvas context');
+    }
+    context.drawImage(img, 0, 0);
+
+    // Convert canvas to a blob
+    const blob = await new Promise<Blob | null>((resolve) => canvas.toBlob(resolve, 'image/png'));
+    if (!blob) {
+      throw new Error('Failed to create image blob');
+    }
+
+    // Copy the image blob to clipboard
+    const clipboardItem = new ClipboardItem({ 'image/png': blob });
+    await navigator.clipboard.write([clipboardItem]);
+
+    console.log('Image copied to clipboard!');
+  } catch (error) {
+    console.error('Error copying image to clipboard:', error);
+  }
+};
